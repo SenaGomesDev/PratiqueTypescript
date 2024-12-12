e1caa0ba931f8c9a8c936afbe1a7014f4c235376
Senagomesdev <Senagomes.dev@gmail.com>
2024-12-12 15:12:33 -0300

    Subida de arquivos

---
+++ src/Atividade.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+}
+function saudacao(name) {
+    return `Olá ${name}`;
+}
+console.log(multiplicar(5, 3));
+console.log(saudacao("Matheus"));
+
+
---
+++ src/Atividade.ts
@@ -0,0 +1,15 @@
+
+function multiplicar(numberOne: number, numberTwo: number): number {
+    return numberOne * numberTwo;
+}
+
+
+function saudacao(name: string): string {
+    return `Olá ${name}`;
+}
+
+
+console.log(multiplicar(5, 3)); 
+console.log(saudacao("Matheus")); 
+
+
